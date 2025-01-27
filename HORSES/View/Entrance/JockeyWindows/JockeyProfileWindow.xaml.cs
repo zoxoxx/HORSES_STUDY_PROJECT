8a17956520ab92b80072ed0ca48d20464cfcdfdd
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-01-27 18:06:03 +0300

    Added a window for entering information about the jockey

---
+++ HORSES/View/Entrance/JockeyWindows/JockeyProfileWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HORSES.View.Entrance.JockeyWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для JockeyProfileWindow.xaml
+    /// </summary>
+    public partial class JockeyProfileWindow : Window
+    {
+        public JockeyProfileWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Данные успешно сохранены!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
