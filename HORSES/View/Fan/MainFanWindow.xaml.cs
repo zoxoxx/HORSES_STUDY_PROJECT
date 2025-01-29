66d4470bd2a9cbdfb1cb8ed67a3b829c1c5bd858
zoxoxx <spaorto740@gmail.com>
2025-01-29 22:42:32 +0300

    Added async mode in new handlers

--- HORSES/View/Fan/MainFanWindow.xaml.cs
+++ HORSES/View/Fan/MainFanWindow.xaml.cs
@@ -38,21 +38,21 @@ namespace HORSES.View.Fan
             window.Show();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
             CompetitionAndRacesWindow window = new CompetitionAndRacesWindow(true);
             this.Close();
             window.Show();
         }
 
-        private void BTN_DONATION_Click(object sender, RoutedEventArgs e)
+        private async void BTN_DONATION_Click(object sender, RoutedEventArgs e)
         {
             DonationWindow window = new DonationWindow();
             this.Close();
             window.Show();
         }
 
-        private void BTN_RESULT_Click(object sender, RoutedEventArgs e)
+        private async void BTN_RESULT_Click(object sender, RoutedEventArgs e)
         {
             CompetitionAndRacesWindow window = new CompetitionAndRacesWindow(true);
             this.Close();
